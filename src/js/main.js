7483ab823da1c204ce464e9d9a408e0a86f0abba
Scott Humphries <scott@sscotth.io>
2015-06-05 14:12:26 -0500

    movin' on

---
+++ src/js/main.js
@@ -0,0 +1,94 @@
+angular
+  .module('angularAddresses', ['ngRoute'])
+
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/landing.html'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html'
+      })
+      .when('/people', {
+        templateUrl: 'views/people.html',
+        controller: 'Main',
+        controllerAs: 'main'
+      })
+      .when('/people/:id', {
+        templateUrl: 'views/person.html',
+        controller: 'PersonController',
+        controllerAs: 'person'
+      })
+      .otherwise({
+        templateUrl: 'views/404.html'
+      });
+  })
+
+  .filter('objToArr', function () {
+    return function (obj) {
+      if (obj) {
+        return Object
+          .keys(obj)
+          .map(function (key) {
+            obj[key]._id = key;
+            return obj[key];
+          });
+      }
+    }
+  })
+
+  .filter('ransomcase', function () {
+    return function (string) {
+      return string
+        .split('')
+        .map(function (char, i) {
+          return i % 2 ? char.toUpperCase() : char.toLowerCase();
+        })
+        .join('');
+    }
+  })
+
+  .controller('PersonController', function ($http, $routeParams) {
+    var vm = this;
+    var id = $routeParams.id;
+
+    $http
+      .get(`https://dontbreakthistime.firebaseio.com/people/${id}.json`)
+      .success(function (data) {
+        vm.data = data;
+      });
+  })
+
+  .controller('Main', function ($http) {
+    var vm = this;
+
+    $http
+      .get('https://dontbreakthistime.firebaseio.com/people.json')
+      .success(function (data) {
+        vm.people = data;
+      });
+
+    vm.newPerson = {};
+
+    vm.addNewAddress = function () {
+      $http
+        .post('https://dontbreakthistime.firebaseio.com/people.json', vm.newPerson)
+        .success(function (res) {
+          vm.people[res.name] = vm.newPerson;
+          vm.newPerson = {};
+          $('#modal').modal('hide');
+        });
+    };
+
+    vm.removeAddress = function (id) {
+      $http
+        .delete(`https://dontbreakthistime.firebaseio.com/people/${id}.json`)
+        .success(function () {
+          delete vm.people[id]
+        });
+    };
+
+  });
